bb0d11a975c663a4b79907aab7c044d0286a4e4d
Ryuma Anai <61266117+ryumaanai@users.noreply.github.com>
2026-04-01 18:48:14 +0900

    型変更

--- app/api/user.ts
+++ app/api/user.ts
@@ -24,7 +24,7 @@ interface UserResponse {
 
 interface UserTweetsResponse {
   tweets: TweetData[];
-  user: User;
+  user: UserData[];
 }
 
 export const login = async (credentials: LoginCredentials): Promise<UserResponse> => {
